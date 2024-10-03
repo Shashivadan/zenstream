ceb8ddb3229b2a331c2b4e0a868151753fdeea51
Shashivadan <shashivadan99@gmail.com>
2024-10-03 14:39:48 +0530

    added: anime card zoom in card

--- src/components/stream-containers/anime-section/episode-content/anime-card.tsx
+++ src/components/stream-containers/anime-section/episode-content/anime-card.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import React from "react";
 import type { IAnimeInfo } from "@/types";
-
+import { motion } from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
 import { ImageIcon } from "lucide-react";
@@ -8,31 +10,48 @@ import { ImageIcon } from "lucide-react";
 
 export default function AnimeCard({ anime }: { anime: IAnimeInfo }) {
   return (
-    <Link href={`/anime/${anime.id}`} className="  border  border-none">
-      <div className="relative flex aspect-video h-[300px] w-full items-center justify-center overflow-hidden rounded-md border border-none bg-background/50 shadow">
+    <Link href={`/anime/${anime.id}`} className="border-none">
+      <motion.div
+        className="relative flex aspect-video h-[300px] w-full items-center justify-center overflow-hidden rounded-md border-none bg-background/50 shadow"
+        whileHover="hover"
+      >
         {anime.image ? (
           <>
-            <Image
-              fill
-              className="h-full w-full object-cover"
-              src={anime.image}
-              alt={anime.title.english ?? ""}
-              sizes="100%"
-            />
+            <motion.div
+              className="relative h-full w-full"
+              variants={{
+                hover: { scale: 1.1 },
+              }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+              <Image
+                fill
+                className="object-cover"
+                src={anime.image}
+                alt={anime.title.english ?? ""}
+                sizes="100%"
+              />
+            </motion.div>
             <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
-            <div className="absolute bottom-2 left-2 right-4 text-white">
+            <motion.div
+              className="absolute bottom-2 left-2 right-4 text-white"
+              variants={{
+                hover: { y: -5 },
+              }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
               <h2 className="font-bold">
                 {anime.title.english ?? anime.title.romaji}
               </h2>
               {anime.releaseDate && (
                 <p className="text-sm opacity-80">{anime.releaseDate}</p>
               )}
-            </div>
+            </motion.div>
           </>
         ) : (
           <ImageIcon className="text-muted" />
         )}
-      </div>
+      </motion.div>
     </Link>
   );
 }
