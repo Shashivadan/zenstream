e2d3a5275809d1bc83e0990336603a8beba56685
Shashivadan <118878507+Shashivadan@users.noreply.github.com>
2024-10-04 20:33:56 +0530

    Update tv-shows-card.tsx

--- src/components/stream-containers/tv-shows-section/tv-shows-contents/tv-shows-card.tsx
+++ src/components/stream-containers/tv-shows-section/tv-shows-contents/tv-shows-card.tsx
@@ -17,7 +17,7 @@ export default function TvShowsCard({ data }: { data: ITvTypes  }) {
             <img
               loading="lazy"
               className="h-full w-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
-              src={`https://sup-proxy.zephex0-f6c.workers.dev/api-content?url=https://image.tmdb.org/t/p/original${data.backdrop_path}`}
+              src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
               alt={data.title || data.name}
               sizes="100%"
             />
