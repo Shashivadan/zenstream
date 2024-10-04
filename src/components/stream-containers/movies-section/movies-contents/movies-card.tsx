b20e570e0b256567a195e26336518dccc8d46763
Shashivadan <118878507+Shashivadan@users.noreply.github.com>
2024-10-04 20:27:18 +0530

    Update movies-card.tsx

--- src/components/stream-containers/movies-section/movies-contents/movies-card.tsx
+++ src/components/stream-containers/movies-section/movies-contents/movies-card.tsx
@@ -15,7 +15,7 @@ export default function MoviesCard({ data }: { data: IMovieTypes }) {
             <Image
               fill
               className="h-full w-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110"
-              src={`https://sup-proxy.zephex0-f6c.workers.dev/api-content?url=https://image.tmdb.org/t/p/original${data.backdrop_path}`}
+              src={`https://image.tmdb.org/t/p/original${data.backdrop_path}`}
               alt={data.title}
               sizes="100%"
             />
